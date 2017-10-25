749d12dc87b2696e971cf4938889dcb5a77da485
Toshiki Jahja <toshikijahja@Toshikis-MBP.hsd1.ca.comcast.net>
2017-10-25 01:13:47 -0700

    [BE-5] Add unit test and settings file

---
+++ src/main/java/com/cronos/Constants.java
@@ -0,0 +1,11 @@
+package com.cronos;
+
+/**
+ * Created by toshikijahja on 10/25/17.
+ */
+public final class Constants {
+
+    // file path
+    public static final String SETTINGS_FILE_PATH = "settings.properties";
+
+}
